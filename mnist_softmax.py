efe2049945da524c54e68cccb38d4a21e4d4e2cb
luowei <luv135@163.com>
2017-11-13 23:00:32 +0800

    mnist

--- mnist_softmax.py
+++ mnist_softmax.py
@@ -1,2 +1,15 @@
+# https://www.tensorflow.org/get_started/mnist/beginners
+
 from tensorflow.examples.tutorials.mnist import input_data
-mnist = input_data.read_data_sets("MNIST_data/",one_hot=True)
+import tensorflow as tf
+
+mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+
+x = tf.placeholder(tf.float32, [None, 487])
+
+W = tf.Variable(tf.zeros([487, 10]))
+b = tf.Variable(tf.zeros([10]))
+
+y = tf.nn.softmax(tf.matmul(x, W) + b)
+
+
